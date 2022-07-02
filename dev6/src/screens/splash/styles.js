7688428ef0999eda549d63c07e8218b47e5221a5
Gabriel <gabrieltsf10@gmail.com>
2022-07-02 14:27:28 -0300

    Adicionando animações na Splash Screen

--- dev6/src/screens/splash/styles.js
+++ dev6/src/screens/splash/styles.js
@@ -1,25 +1,37 @@
 import styled from "styled-components/native";
 
 
+export const FadeContainer = styled.View`
+    margin-left:6.5em;
+    margin-top:4em;
+    background-color:black;
+`;
 
+export const Subtitulo = styled.Text`
+    font-size: 48px;
+    font-family: "Poppins";
+    color: white;
+`;
 
 export const Container = styled.View`
    background-color: black;
-    
+   display:block;
 `;
 
 export const Logo = styled.Image`
 position:absolute;
 width: 100%;
-height: 50em;
+height: 55rem;
+background-repeat:repeat;
+background-color: black;
 z-index:-1;
 `;
 
 export const Imagem = styled.Image`
-    margin-left: 6.75em;
-    margin-top:12em;
-    width:10em;
-    height: 10em;
+    margin-top: 55%;
+    margin-left: 30%;
+    width:10rem;
+    height: 10rem;
     z-index:-1;
 `;
 
@@ -28,7 +40,9 @@ export const TextoInput = styled.View`
     font-family: "Poppins";
     font-size:64px;
     color:white;
-    margin-left:1.38em;
-    margin-top: 0.15em;
+    margin-left: 25%;
+    margin-top: 0.15rem;
     z-index: 5;
 `;
+
+
