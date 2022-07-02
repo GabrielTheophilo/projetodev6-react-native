1031fc1a54547bf097ba8dc6e900b540e089c480
RosanaDantas <rfdrosana@gmail.com>
2022-07-02 14:57:36 -0300

    Implementação component LogoWithTitle

---
+++ dev6/src/components/LogoWithTitle/Index.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { Container } from "./styles";
+import { Content } from "./styles";
+import { Imagem } from "./styles";
+import { Title } from "./styles";
+
+
+
+export const LogoWithTitle = (props) => {
+    return (
+      <Container>
+        <Content>
+        <Imagem source={require("../../../assets/icon.png")} />
+        <Title>{props.title}</Title>
+        </Content>
+      </Container>
+    );
+  };
+  
+  export default LogoWithTitle;
---
+++ dev6/src/components/LogoWithTitle/styles.js
@@ -0,0 +1,36 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    width: 90%;
+    align-self: center;
+    padding: 15px;
+`;
+
+export const Content = styled.View`
+    flex-direction: row;
+    padding: 10px;
+
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  color: #000;
+  font-weight: bold;
+  align-self: center;
+  padding-top: 3px;
+  padding-bottom: 3px;
+`;
+
+export const Logo = styled.Image`
+position:absolute;
+width: 100%;
+height: 50em;
+z-index:-1;
+`;
+ 
+export const Imagem = styled.Image`
+    margin-right: 20px;
+    width:45;
+    height: 45;
+    z-index:-1;
+`;
