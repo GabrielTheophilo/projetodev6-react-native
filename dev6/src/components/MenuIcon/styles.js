6d9b1496f6655955be49f46daba89d1592a230cd
RosanaDantas <rfdrosana@gmail.com>
2022-07-03 19:54:55 -0300

    Implementação component MenuIcon

---
+++ dev6/src/components/MenuIcon/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const Title = styled.Text`
+  font-size: 16px;
+  color: #000;
+  font-weight: bold;
+  padding-top: 3px;
+  padding-bottom: 3px;
+
+`;
+
+export const Touchable = styled.TouchableOpacity`
+  align-items:center;
+  padding-top:5px;
+  padding-bottom: 5px;
+`;
