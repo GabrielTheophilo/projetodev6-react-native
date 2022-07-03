29792643826ac19aed18e73f27ca0de5784734d9
RosanaDantas <rfdrosana@gmail.com>
2022-07-03 19:58:51 -0300

    Removendo comentário do código.

--- dev6/src/components/MenuIcon/Index.js
+++ dev6/src/components/MenuIcon/Index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Container, Touchable, Title } from "./styles";
+import { Touchable, Title } from "./styles";
 
 export const MenuIcon = (props) => {
   return (
-    // <Container>
-      <Touchable onPress={props.onPress}>
-        <Ionicons name={props.iconName} size={60} color="orange" />
-        <Title>{props.name}</Title>
-      </Touchable>
-    // </Container>
+    <Touchable onPress={props.onPress}>
+      <Ionicons name={props.iconName} size={60} color="orange" />
+      <Title>{props.name}</Title>
+    </Touchable>
   );
 };
 
