dbd3fa046fe71ca73baf267d7c8fab439684f6f1
RosanaDantas <rfdrosana@gmail.com>
2022-07-03 18:50:22 -0300

    Implementação component InputText

---
+++ dev6/src/components/InputText/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextInput } from "./styles";
+
+export const InputText = ({ placeholder }) => {
+  return (
+    <>
+      <TextInput placeholder={placeholder} />
+    </>
+  );
+};
+
+export default InputText;
---
+++ dev6/src/components/InputText/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const TextInput = styled.TextInput`
+  background-color: white;
+  font-size: 14px;
+  border-radius: 5px;
+  border-width: 2px;
+  border-color: #dcdcdc;
+  width:  100%;
+  height: 35px;
+  padding: 5px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  align-self: center;
+`;
