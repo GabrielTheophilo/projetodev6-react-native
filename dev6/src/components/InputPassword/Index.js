c40fdd13d49e1f8706ebe1197ea86cf29877ffa6
RosanaDantas <rfdrosana@gmail.com>
2022-07-04 19:48:57 -0300

    Implementação component InputPassword.

---
+++ dev6/src/components/InputPassword/Index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+const InputPassword = ({ placeholder }) => {
+  const [input, setInput] = useState("");
+  const [hidePass, setHidePass] = useState(false);
+
+  const onPress = () => {
+    setHidePass(!hidePass);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputArea}>
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          value={input}
+          onChangeText={(text) => setInput(text)}
+          secureTextEntry={hidePass}
+        />
+        <TouchableOpacity style={styles.icon} onPress={onPress}>
+          {hidePass ? (
+            <Ionicons name="eye-off" size={26} color="#cdcdcd" />
+          ) : (
+            <Ionicons name="eye" size={26} color="#cdcdcd" />
+          )}
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default InputPassword;
