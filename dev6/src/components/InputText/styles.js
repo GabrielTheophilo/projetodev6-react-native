cc00bfa3bf2636860877d26a4d9255bdd9958a45
RosanaDantas <rfdrosana@gmail.com>
2022-07-04 20:12:04 -0300

    Ajuste tamanho placeholder do inputText

--- dev6/src/components/InputText/styles.js
+++ dev6/src/components/InputText/styles.js
@@ -2,13 +2,13 @@ import styled from "styled-components/native";
 
 export const TextInput = styled.TextInput`
   background-color: white;
-  font-size: 14px;
+  font-size: 18px;
   border-radius: 5px;
   border-width: 2px;
   border-color: #dcdcdc;
   width:  100%;
   height: 35px;
-  padding: 5px;
+  padding: 8px;
   margin-top: 5px;
   margin-bottom: 5px;
   align-self: center;
