63f61de70242878593219dd6fd3083662d2e3989
RosanaDantas <rfdrosana@gmail.com>
2022-07-04 21:09:39 -0300

    Ajuste UserImage

--- dev6/src/components/UserImage/Index.js
+++ dev6/src/components/UserImage/Index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
-
-export const UserImage = (props) => {
+export const UserImage = (photo) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.photoContainer} disabled>
-      <Ionicons name="md-download-outline" size={30} color="#797878" />
-      </TouchableOpacity>
+      {photo === true ? (
+        <Image style={styles.photo} source={photo}/>
+      ) : (
+        <TouchableOpacity style={styles.photoContainer} disabled>
+          <Ionicons name="md-download-outline" size={25} color="#797878" />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
