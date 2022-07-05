5e097e0f6d9de33e1eed60ffd93573fa9fe113de
RosanaDantas <rfdrosana@gmail.com>
2022-07-05 18:15:25 -0300

    Inclusão de setTimeout na splash

--- dev6/src/screens/splash/index.js
+++ dev6/src/screens/splash/index.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useRef } from "react";
 import { Text, Button, Pressable } from "react-native";
 import { FadeInView } from "../../components/animation";
-import { Logo,Subtitle,LoadingContainer, Container, Image, TextInput, FadeContainer } from './styles';
-import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import {
+  Logo,
+  Subtitle,
+  LoadingContainer,
+  Container,
+  Image,
+  TextInput,
+  FadeContainer,
+} from "./styles";
+import { useFonts, Poppins_700Bold } from "@expo-google-fonts/poppins";
 import { PageLoading } from "../../components/pageLoading";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackView } from "@react-navigation/native-stack";
 
+const Splash = () => {
+  const navigation = useNavigation();
 
-
-
-export const Splash = () => {
+  useEffect(() => {
+    setTimeout(function () {
+      navigation.navigate("Login");
+    }, 4000);
+  });
 
   let [fontsLoaded] = useFonts({
     Poppins_700Bold,
@@ -20,26 +32,22 @@ export const Splash = () => {
       <LoadingContainer>
         <PageLoading />
       </LoadingContainer>
-    )
-  }else {
-  return (
-   
-    <Container>  
-       
-      <Logo source={require("../../../assets/img/fundo.png")} />
-      <FadeInView>
-        <Image source={require("../../../assets/icon.png")} />
-        <TextInput>Dev-6</TextInput>
-        
-        <FadeContainer>
-          
-          <Subtitle/>
-        </FadeContainer>
-      </FadeInView>
+    );
+  } else {
+    return (
+      <Container>
+        <Logo source={require("../../../assets/img/fundo.png")} />
+        <FadeInView>
+          <Image source={require("../../../assets/icon.png")} />
+          <TextInput>Dev-6</TextInput>
 
-    </Container>
-      
-  );}
+          <FadeContainer>
+            <Subtitle />
+          </FadeContainer>
+        </FadeInView>
+      </Container>
+    );
+  }
 };
 
 export default Splash;
