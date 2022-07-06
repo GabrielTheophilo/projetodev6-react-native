3ab3b108fa347d9a0dfc3619e636a56042677dc8
isabeels <101607831+isabeels@users.noreply.github.com>
2022-07-05 22:41:45 -0300

    Alteracoes

---
+++ dev6/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://reactnative.herokuapp.com/reactnative",
+  auth: {
+    username: "reactnative",
+    password: "2022",
+  },
+});
+
+export default api;
