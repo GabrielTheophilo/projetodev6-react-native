410adbf3047f110001ded3525b711aa289513e7a
isabeels <101607831+isabeels@users.noreply.github.com>
2022-07-06 18:20:39 -0300

    Adiciona metodo Post em usuario

--- dev6/src/screens/UserRegistration/index.js
+++ dev6/src/screens/UserRegistration/index.js
@@ -1,28 +1,83 @@
 import { Container, Content } from "./../../components/GlobalStyles/styles";
 import LogoWithTitle from "../../components/LogoWithTitle";
-import UserImage from "../../components/UserImage";
-import InputText from "../../components/InputText";
 import ButtonBack from "../../components/ButtonBack";
 import Button from "../../components/Button";
-import InputPassword from "../../components/InputPassword";
+import { postUser } from "../../services/users";
+import { UserText } from "./styles";
+import { useState } from "react";
+import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
 
 const UserRegistration = () => {
-  const nav = useNavigation();
-  function backScreen() {
-    nav.goBack();
-  }
+  useFonts({
+    Poppins_400Regular,
+  });
+
+  const [usuarioNome, setUsuarioNome] = useState("");
+  const [usuarioFoto, setUsuarioFoto] = useState("");
+  const [usuarioCpf, setUsuarioCpf] = useState("");
+  const [usuarioDtNascimento, setUsuarioDtNascimento] = useState("");
+  const [usuarioLogin, setUsuarioLogin] = useState("");
+  const [usuarioSenha, setUsuarioSenha] = useState("");
+
+  const newUser = {
+    id: 123,
+    nome: usuarioNome,
+    foto: usuarioFoto,
+    dtNascimento: usuarioDtNascimento,
+    login: usuarioLogin,
+    senha: usuarioSenha,
+    cpf: usuarioCpf,
+  };
+
+  const HandlePost = () => {
+    postUser(newUser);
+  };
+
+  // const nav = useNavigation();
+  // function backScreen() {
+  //   nav.goBack();
+  // }
+
   return (
     <Container>
       <Content>
         <LogoWithTitle title="cadastrar usuário" />
-        <UserImage />
-        <InputText placeholder="nome completo" />
-        <InputText placeholder="cpf" />
-        <InputText placeholder="data de nascimento" />
-        <InputText placeholder="login" />
-        <InputPassword placeholder="senha" onPress={() => onPress} />
-        <Button onPress={() => console.log("handleToSave")} name="salvar" />
-        <ButtonBack onPress={backScreen}/>
+      </Content>
+      <Content>
+        <UserText
+          placeholder="foto"
+          value={usuarioFoto}
+          onChangeText={setUsuarioFoto}
+        />
+        <UserText
+          placeholder="nome"
+          value={usuarioNome}
+          onChangeText={setUsuarioNome}
+        />
+        <UserText
+          placeholder="cpf"
+          value={usuarioCpf}
+          onChangeText={setUsuarioCpf}
+        />
+        <UserText
+          placeholder="data de nascimento"
+          value={usuarioDtNascimento}
+          onChangeText={setUsuarioDtNascimento}
+        />
+        <UserText
+          placeholder="login"
+          value={usuarioLogin}
+          onChangeText={setUsuarioLogin}
+        />
+        <UserText
+          placeholder="senha"
+          value={usuarioSenha}
+          onChangeText={setUsuarioSenha}
+        />
+      </Content>
+      <Content>
+        <Button onPress={HandlePost} name="salvar" />
+        <ButtonBack />
       </Content>
     </Container>
   );
---
+++ dev6/src/screens/UserRegistration/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const UserText = styled.TextInput`
+  background-color: white;
+  font-size: 15px;
+  border-radius: 5px;
+  border-width: 2px;
+  border-color: #dcdcdc;
+  width: 100%;
+  height: 35px;
+  padding: 8px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  align-self: center;
+  font-family: "Poppins_400Regular";
+`;
