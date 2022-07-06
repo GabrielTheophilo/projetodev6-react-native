3f72f2d538d64da5e91e6244d6322d05e53bd659
RosanaDantas <rfdrosana@gmail.com>
2022-07-05 21:05:22 -0300

    Implementação component CardItem

---
+++ dev6/src/components/CardItem/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Text,
+  Container,
+  Image,
+  Content,
+  View,
+  Content2,
+  View2,
+  TouchableOpacity,
+} from "./styles";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const CardItem = ({ category }) => {
+  const { nome, foto } = category;
+
+  return (
+    <Container>
+      <View2>
+        <Content>
+          <Image source={foto}></Image>
+          <Text>{nome}</Text>
+        </Content>
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              onPress;
+            }}
+          >
+            <Ionicons name="trash" size={26} color="#ff7800" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              onPress;
+            }}
+          >
+            <Ionicons name="ios-pencil" size={26} color="#ff7800" />
+          </TouchableOpacity>
+        </View>
+      </View2>
+    </Container>
+  );
+};
+
+export default CardItem;
---
+++ dev6/src/components/CardItem/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  margin-left: 40;
+`;
+
+export const Image = styled.Image`
+  width: 135px;
+  border-radius: 15;
+  height: 135px;
+  border-color: "#cdcdcd";
+  border: 1px;
+`;
+
+export const Text = styled.Text`
+  font-size: 14px;
+  color: #000;
+  font-family: "Poppins_500Medium";
+  padding-top: 3px;
+  padding-bottom: 3px;
+`;
+
+export const Content = styled.View`
+  flex-direction: column;
+  align-items: center;
+`;
+export const Content2 = styled.View``;
+
+export const View = styled.View`
+  margin: 10px;
+`;
+export const View2 = styled.View`
+  flex-direction: row;
+`;
+
+export const TouchableOpacity = styled.TouchableOpacity`
+
+`;
