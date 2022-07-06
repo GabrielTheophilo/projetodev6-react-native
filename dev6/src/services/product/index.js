b4fc6d32685d26e7ba678624f2a78cbc12f93da2
Vinícius Soares <vinivsp2304@hotmail.com>
2022-07-05 22:52:02 -0300

    merge
    merge branch main com a minha branch ViniVini

---
+++ dev6/src/services/product/index.js
@@ -0,0 +1,25 @@
+import Api from "../api"
+
+export const getProduct = () => {
+
+​	return Api.get('/produto').then((response) => response.data)
+
+}; 
+
+export const deleteProduct = () =>{
+
+​	return Api.delete('/produto').then((response) => response.data)
+
+}; 
+
+export const postProduct = () => {
+
+​	return Api.post('/produto').then((response) => response.data)
+
+}; 
+
+export const putProduct = () => {
+
+​	return Api.put('/produto').then((response) => response.data)
+
+}; 
